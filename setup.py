efcb52952a8dc42d7e71e15e3db5282d2adfcd67
Stefan Sullivan <stefan.sullivan@smule.com>
2019-09-03 11:56:05 -0700

    requirements were copied from requirements.txt, which tends to be strict about requirements even when a specific version is not required. Let's just declare _only_ the requirements which are needed in src/ and delete versions until we know that something is incompatible

--- setup.py
+++ setup.py
@@ -19,16 +19,11 @@ if __name__ == '__main__':
 
           # Describe dependencies.
           install_requires=[
-              'dill == 0.2.8.2',
-              'h5py == 2.8.0',
-              'Keras == 2.1.1',
-              'librosa == 0.5.1',
-              'matplotlib == 2.2.3',
-              'numpy == 1.14.5',
-              'pandas == 0.23.4',
-              'scikit-learn == 0.20.0',
-              'scipy == 1.1.0',
-              'seaborn == 0.9.0',
-              'tensorflow == 1.10.0',
+              'Keras',
+              'numpy',
+              'scipy',
+              'scikit-learn',
+              'matplotlib',
+              'librosa',
           ],
           )
